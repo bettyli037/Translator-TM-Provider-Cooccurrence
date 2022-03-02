0b3e35c173ee7648a62c5587342196ced93e847b
edgargaticaCU <88678635+edgargaticaCU@users.noreply.github.com>
2022-03-02 12:09:53 -0600

    Adding the title and document metrics.

---
+++ src/main/java/edu/ucdenver/ccp/cooccurrence/DocumentCount.java
@@ -0,0 +1,36 @@
+package edu.ucdenver.ccp.cooccurrence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "document_counts")
+public class DocumentCount {
+
+    @Column(name = "document_part")
+    @NotEmpty
+    @Id
+    private String part;
+
+    @Column(name = "record_count")
+    private int recordCount;
+
+    public String getPart() {
+        return part;
+    }
+
+    public void setPart(String part) {
+        this.part = part;
+    }
+
+    public int getRecordCount() {
+        return recordCount;
+    }
+
+    public void setRecordCount(int count) {
+        this.recordCount = count;
+    }
+}
